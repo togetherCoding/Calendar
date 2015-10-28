dd17888af7f63a70bdc8bf7e991a01c064e43a95
lukaszreszke93 <lukaszreszke93@gmail.com>
2015-10-28 23:15:06 +0100

    Pushbuttons
    According to tasklist, I've made 7 PushButton's representing every day
    of week.
    I've also removed startLabel cause as we already go further it's
    useless.

--- sources/launcher.cpp
+++ sources/launcher.cpp
@@ -6,10 +6,25 @@ int main(int argc, char* argv[])
 
     QWidget *mainWindow = new QWidget;                      // perhaps we have to create new class (e.g. Calendar) which will derive from QWidget
 
-    QLabel *startLabel = new QLabel("No to zaczynamy!");
+
+    QPushButton *dayOne = new QPushButton("Monday");                  // makes dynamic pointer to QPushButton class (am I right? still newb) representing days of week
+    QPushButton *dayTwo = new QPushButton("Tuesday");
+    QPushButton *dayThree = new QPushButton("Wednesday");
+    QPushButton *dayFour = new QPushButton("Thursday");
+    QPushButton *dayFive = new QPushButton("Friday");
+    QPushButton *daySix = new QPushButton("Saturday");
+    QPushButton *daySeven = new QPushButton("Sunday");
 
     QHBoxLayout *layout = new QHBoxLayout;                  // we could use just QBoxLayout, which is parent of QH... and QV..., but it requires defining direction anyway
-    layout->addWidget(startLabel);
+
+    layout->addWidget(dayOne);
+    layout->addWidget(dayTwo);
+    layout->addWidget(dayThree);
+    layout->addWidget(dayFour);
+    layout->addWidget(dayFive);
+    layout->addWidget(daySix);
+    layout->addWidget(daySeven);
+
 
     mainWindow->setLayout(layout);
 
